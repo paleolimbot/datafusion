4c51db21c3ea11dbe5f52fa4d2eb6bfa45bf343b
Dewey Dunnington <dewey@fishandwhistle.net>
2022-11-25 13:50:34 -0400

    add test for the lazy sql generation

--- tests/testthat/test-datafusion-sql.R
+++ tests/testthat/test-datafusion-sql.R
@@ -2,3 +2,9 @@
 test_that("sql variant can be created", {
   expect_s3_class(dbplyr::sql_translation(simulate_datafusion()), "sql_variant")
 })
+
+test_that("dbplyr::lazy_frame() can generate some SQL from an expression", {
+  lf <- dbplyr::lazy_frame(a = TRUE, b = 1, c = 2, d = "z", con = simulate_datafusion())
+  lf_agg <- dplyr::summarise(lf, x = sd(b, na.rm = TRUE))
+  expect_match(as.character(dbplyr::sql_render(lf_agg)), "^SELECT STDDEV_SAMP")
+})
